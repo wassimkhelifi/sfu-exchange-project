ccf10fe197359890975daabe9a3cb33969b7353a
ggiovani <ggiovani@sfu.ca>
2021-11-25 20:23:57 -0800

    add Profile page skeleton
    Signed-off-by: ggiovani <ggiovani@sfu.ca>
    
    delete .DS_Store
    
    Signed-off-by: ggiovani <ggiovani@sfu.ca>
    
    pull user data from a randomly seeded user
    
    prettify user_helper.py
    
    address PR comments on CSS variables / import

--- sfu-exchange-project/exchange/views/profile.py
+++ sfu-exchange-project/exchange/views/profile.py
@@ -1,10 +1,28 @@
+import random
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from ..helpers import user_helper
+from ..models import User, Question
 
 
 
 def ProfileView(request):
-    return HttpResponse('<h1>Profile Page</h1>')
+    # choose a question, get its user, use that user for the demo; TODO: remove
+    q_pks = Question.objects.values_list('pk', flat=True)
+    q_pk = random.choice(q_pks)
+    q = Question.objects.get(pk=q_pk)
+
+    user = User.objects.get(pk=q.user_id.id)
+    questions = user_helper.get_user_top_questions(user)
+    tags = user_helper.get_user_top_tags(user)
+    user_helper.format_user(user)
+
+    context = { 
+        'user': user or {},
+        'questions': questions or {},
+        'tags': tags or {}
+    }
+    return render(request, 'exchange/profile.html', context)
 
 def ProfileEditView(request):
-    return HttpResponse('<h1>Profile Edit Page</h1>')
+    return render(request, 'exchange/profileEdit.html')
