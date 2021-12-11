f50f497a2287b2722e1a6200e1883792f52ba04e
Nirvon <nshoa@sfu.ca>
2021-12-10 16:36:22 -0800

    Add default role for students

--- sfu-exchange-project/exchange/views/register.py
+++ sfu-exchange-project/exchange/views/register.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from ..forms import RegistrationForm
 from django.contrib.auth import login
 from django.contrib import messages
+from ..models import Role
 
 
 def RegisterView(request):
@@ -13,7 +14,13 @@ def RegisterView(request):
         print('@@POST: form', form)
         if form.is_valid():
             user = form.save()
-            #login(request, user)
+            
+            # Add a default role here, as ManyToManyField does not allow default values
+            student_role = Role.objects.filter(name='Student').first()
+            user.roles.add(student_role)
+            user.save()
+            
+            login(request, user)
             messages.success(request, "Registration successful." )
             return redirect("Home")
         messages.error(request, "Unsuccessful registration. Invalid information.")
@@ -21,4 +28,3 @@ def RegisterView(request):
         print("@@@@@@@@@@@@ NOT INSIDE POST @@@@@@@@@@@@")
         form = RegistrationForm()
     return render (request=request, template_name="exchange/register.html", context={"form":form})
-    # return render(request, 'exchange/register.html')
