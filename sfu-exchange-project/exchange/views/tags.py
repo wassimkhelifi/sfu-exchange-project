83dea4f74bd7a34d803547e7975da32f4934f606
ggiovani <ggiovani@sfu.ca>
2021-12-12 13:14:05 -0800

    add pagination to tags page
    Signed-off-by: ggiovani <ggiovani@sfu.ca>
    
    correct spelling
    
    Signed-off-by: ggiovani <ggiovani@sfu.ca>

--- sfu-exchange-project/exchange/views/tags.py
+++ sfu-exchange-project/exchange/views/tags.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Paginator 
 
 from ..models import Tag
 
 # Creating the tags view
 def TagsView(request):
-    tags_list = Tag.objects.all()
+    tags = Tag.objects.all()
+
+    paginator = Paginator(tags, 12)
+
+    page = request.GET.get('page')
+    paginated_tags = paginator.get_page(page)
+
     return render(request, 'exchange/tags.html', {
-        'tags_list': tags_list,
+        'tags_list': paginated_tags,
     })
