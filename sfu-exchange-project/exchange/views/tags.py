fa25594bc3f70501085690044da6fa9a0f146ee4
ggiovani <ggiovani@sfu.ca>
2021-12-12 13:32:30 -0800

    add search
    Signed-off-by: ggiovani <ggiovani@sfu.ca>

--- sfu-exchange-project/exchange/views/tags.py
+++ sfu-exchange-project/exchange/views/tags.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator 
+from django.db.models import Q
+from django.shortcuts import render
 
 from ..models import Tag
 
 # Creating the tags view
 def TagsView(request):
-    tags = Tag.objects.all()
+    if request.method == "GET":
+        query_params = request.GET.get("q")
 
-    paginator = Paginator(tags, 12)
+        if query_params:
+            tags = Tag.objects.filter(
+                Q(name__icontains=query_params)
+            )
 
-    page = request.GET.get('page')
-    paginated_tags = paginator.get_page(page)
+        else:
+            tags = Tag.objects.all()
 
-    return render(request, 'exchange/tags.html', {
-        'tags_list': paginated_tags,
-    })
+        paginator = Paginator(tags, 12)
+
+        page = request.GET.get('page')
+        paginated_tags = paginator.get_page(page)
+
+        return render(request, 'exchange/tags.html', {
+            'tags_list': paginated_tags,
+        })
