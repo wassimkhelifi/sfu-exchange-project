d7c6754168f8704f8cb039d11bd868bdd465ad3b
Mikhail Alexeev <malexeev@sfu.ca>
2021-12-12 00:09:45 -0800

    Pagination for users

--- sfu-exchange-project/exchange/views/users.py
+++ sfu-exchange-project/exchange/views/users.py
@@ -1,7 +1,19 @@
+from django.core import paginator
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator 
 
-
+from ..models import User 
 
 def UsersView(request):
-    return HttpResponse('<h1>Users Page</h1>')
+    users = User.objects.all()
+    # Default 25 users per page for now
+    paginator = Paginator(users, 25) 
+
+    page = request.GET.get('page')
+    paginated_users = paginator.get_page(page)
+    return render(
+        request, 'exchange/users.html', {
+            'user_list': paginated_users
+        }
+    )
