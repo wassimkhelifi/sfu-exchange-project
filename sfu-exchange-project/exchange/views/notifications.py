26cab4814c3fc118c5f713ee50e4f2a0a6bc8ca8
ddziubii <ddziubii@sfu.ca>
2021-12-13 00:18:41 -0800

    pass notifications list into new page

--- sfu-exchange-project/exchange/views/notifications.py
+++ sfu-exchange-project/exchange/views/notifications.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from ..helpers import notification_helper
 
 
-from ..models import Question
+from ..models import Notification
 
 # Creating the notifications view
 def NotificationsView(request):
-    questions_list = Question.objects.all()
-    # TODO: can this be removed?
     notification_list = notification_helper.get_notifications(request.user)
     return render(request, 'exchange/notifications.html', {
-        'questions_list': questions_list,
-        'notifications': notification_list,
+        "notifications": notification_list,
     })
