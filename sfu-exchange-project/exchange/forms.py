b355856440a2ac283856ce86c774b083242dcdbb
Mikhail Alexeev <malexeev@sfu.ca>
2021-12-12 16:40:22 -0800

    Editing User form seems to be ready

--- sfu-exchange-project/exchange/forms.py
+++ sfu-exchange-project/exchange/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from .models import User, Tag, Answer
+from django.forms import widgets
+from .models import Faculty, User, Tag, Answer
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import get_object_or_404
+
 
 class RegistrationForm(UserCreationForm):
     # Fields we redefine here overwrite those in the form. By default, they are 'required'
@@ -11,25 +13,67 @@ class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2', 'faculty_id', 'bio']
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "password1",
+            "password2",
+            "faculty_id",
+            "bio",
+        ]
+
 
 class QuestionForm(forms.Form):
     title = forms.CharField(max_length=150)
     # TODO: Max length of stackoverflow body is 30k characters, need to change model limit. : https://meta.stackexchange.com/questions/176445/knowing-your-limits-what-is-the-maximum-length-of-a-question-title-post-image
     question_text = forms.CharField(max_length=3000)
-    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple)
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
     user_id = 1
     anonymous = forms.BooleanField(required=False)
-    
+
     # TODO: Potentially provide validation if needed, otherwise we can remove these functions
     def clean_title(self):
-        data = self.cleaned_data['title']
+        data = self.cleaned_data["title"]
         return data
 
     def clean_questions_text(self):
-        data = self.cleaned_data['question_text']
+        data = self.cleaned_data["question_text"]
         return data
+
 
 class AnswerForm(forms.Form):
     answer_text = forms.CharField(max_length=3000)
     anonymous = forms.BooleanField(required=False)
+
+
+IMG_CHOICES = (
+    ("racoon", "Racoon"),
+    ("moose", "Moose"),
+    ("beaver", "Beaver"),
+    ("deer", "Deer"),
+    ("bear", "Bear"),
+)
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "bio",
+            "img",
+            "faculty_id"
+        ]
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control", "placeholder":"First Name"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Last Name"}),
+            "bio": forms.Textarea(attrs={"class": "form-control", "placeholder": "Enter a short bio..."}),
+            "img": forms.Select(choices=IMG_CHOICES, attrs={"class": "form-control"}),
+            "faculty_id": forms.Select(attrs={"class": "form-control"}),
+        }
+
