b4fd936df4ff2b403cff9a7cf601d9405e6efd6c
ggiovani <ggiovani@sfu.ca>
2021-12-12 17:46:24 -0800

    add pagination to profile questions
    Signed-off-by: ggiovani <ggiovani@sfu.ca>

--- sfu-exchange-project/exchange/views/profile.py
+++ sfu-exchange-project/exchange/views/profile.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from ..helpers import user_helper
 from ..models import User
@@ -11,9 +12,13 @@ def ProfileView(request):
     tags = user_helper.get_user_top_tags(user)
     user_helper.format_user(user)
 
+    paginator = Paginator(questions, 2)
+    page = request.GET.get('page')
+    paginated_questions = paginator.get_page(page)
+
     context = { 
         'user': user or {},
-        'questions': questions or {},
+        'questions': paginated_questions or {},
         'tags': tags or {}
     }
     return render(request, 'exchange/profile.html', context)
