bcb1f42f5f6b278f5e093be75886881010ee4e36
ddziubii <ddziubii@sfu.ca>
2021-12-13 11:04:22 -0800

    enable pagination by date created on notifications page
    Signed-off-by: ddziubii <ddziubii@sfu.ca>

--- sfu-exchange-project/exchange/views/notifications.py
+++ sfu-exchange-project/exchange/views/notifications.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.core.paginator import Paginator
+
 from ..helpers import notification_helper
-
-
 from ..models import Notification
 
-# Creating the notifications view
-def NotificationsView(request):
-    notification_list = notification_helper.get_notifications(request.user)
+def NotificationsView(request, page=""):
+    query_param = request.GET.get("q")
+    query_param = "" if not query_param else query_param
+
+    search_vector = SearchVector('notification_title', 'notification_text', 'created_at')
+    search_query = SearchQuery(query_param)
+
+    notifications = notification_helper.get_notifications(request.user).annotate(
+        search=search_vector, 
+        rank=SearchRank(search_vector, search_query),
+    )
+
+    if query_param:
+        notifications = notifications.filter(search=query_param).order_by("-rank", "-created_at")
+    else:
+        notifications = notifications.order_by("-created_at")
+
+    paginator = Paginator(notifications, 10)
+
+    page = request.GET.get("page")
+    notification_list = paginator.get_page(page)
+
     return render(request, 'exchange/notifications.html', {
         "notifications": notification_list,
     })
