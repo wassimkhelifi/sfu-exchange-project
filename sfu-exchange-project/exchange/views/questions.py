4eeaa32be8110b1976ea3761f861eccaed9a4a76
Mikhail Alexeev <malexeev@sfu.ca>
2021-12-12 23:11:09 -0800

    Paginated Questions

--- sfu-exchange-project/exchange/views/questions.py
+++ sfu-exchange-project/exchange/views/questions.py
@@ -1,14 +1,18 @@
+from django.core import paginator
+from django.db.models import query
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# def QuestionsView(request):
-#     return HttpResponse('<h1>Questions Page</h1>')
-
+from django.core.paginator import Paginator
 from ..models import Question
 
 # Creating the questions view
 def QuestionsView(request):
     questions_list = Question.objects.all()
+
+    paginator = Paginator(questions_list, 10)
+    page = request.GET.get("page")
+    paginated_questions = paginator.get_page(page)
+
     return render(request, 'exchange/questions.html', {
-        'questions_list': questions_list,
+        'questions_list': paginated_questions,
     })
