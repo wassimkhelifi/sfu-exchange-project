4c43b3f073f9da570082559a59a4f3b3762cc9cb
Nirvon <nshoa@sfu.ca>
2021-12-12 16:39:21 -0800

    Implement full-text-search
    Upgrade users and questions to perform inclusive-or FTS. Also redirect tags to questions search page

--- sfu-exchange-project/exchange/views/search.py
+++ sfu-exchange-project/exchange/views/search.py
@@ -1,6 +1,7 @@
 from django.db.models import query
 from django.shortcuts import render
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from ..models import Question, User
 
 
@@ -8,16 +9,27 @@ from ..models import Question, User
 # Creating the questions view
 def SearchView(request):
     if request.method == 'GET':
-        query = request.GET.get('q')
-        user_ids = User.objects.filter(
-            Q(username__icontains=query)   | 
-            Q(first_name__icontains=query) | 
-            Q(last_name__icontains=query)
-        )
-        search_results = Question.objects.filter(
-            Q(title__icontains=query) | Q(user_id__in=user_ids)
-        )
+        query_params = request.GET.get('q')
+
+        if query_params:
+            # Search these fields
+            search_vector = SearchVector('title', 'question_text', 'tags__name')
+            
+            # Make the search inclusive OR for each term in the query
+            or_query_params = ' | '.join(query_params.split())
+            search_query = SearchQuery(or_query_params, search_type='raw')
+
+            # Perform the search
+            questions = Question.objects.annotate(
+                search=search_vector, 
+                rank=SearchRank(search_vector, search_query)
+            )
+            
+            questions = questions.filter(search=search_query).order_by("-rank", "-created_at")
+        else:
+            questions = Question.objects.order_by("-created_at")
+
         return render(request, 'exchange/search.html', {
-            'query': query,
-            'search_results': search_results,
+            'query': query_params if query_params != None else "",
+            'search_results': questions,
         })
--- sfu-exchange-project/exchange/views/users.py
+++ sfu-exchange-project/exchange/views/users.py
@@ -5,33 +5,39 @@ from django.core.paginator import Paginator
 from django.db.models import Count, Sum, query
 from django.db.models.functions import Coalesce
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from ..models import User
 
 
 def UsersView(request, page=""):
     if request.method == "GET":
         query_params = request.GET.get("q")
-        # Conditionally query users, otherwise return all
-        if query_params:
-            users = User.objects.filter(
-                Q(username__icontains=query_params)
-                | Q(first_name__icontains=query_params)
-                | Q(last_name__icontains=query_params)
-                | Q(email__icontains=query_params)
-            )
-            print("@@@USERSVIEW: search query:", query_params)
-        else:
-            users = User.objects
-        users = users.annotate(
+
+        # Search these fields
+        search_vector = SearchVector('username', 'email', 'first_name', 'last_name', 'bio', 'roles__name', 'faculty_id__name')
+        
+        # Make the search inclusive OR for each term in the query
+        or_query_params = ' | '.join(query_params.split() if query_params != None else "")
+        search_query = SearchQuery(or_query_params, search_type='raw')
+
+        # Perform the search
+        users = User.objects.annotate(
+            search=search_vector, 
+            rank=SearchRank(search_vector, search_query),
             num_posts=Count("answer__votes"),
             num_answers=Count("answer"),
-            num_points=Coalesce(
-                Sum("answer__votes"), 0
-            ),  # default to 0 if user has no  answers
-        ).order_by('username')
+            num_points=Coalesce(Sum("answer__votes"), 0), # default to 0 if user has no  answers
+        )
+        
+        if query_params:
+            users = users.filter(search=search_query).order_by("-rank", "username")
+        else:
+            users = users.order_by("username")
+
         # Default 25 users per page for now
         paginator = Paginator(users, 24)
 
         page = request.GET.get("page")
         paginated_users = paginator.get_page(page)
-        return render(request, "exchange/users.html", {"user_list": paginated_users, "search_text": query_params})
+        search_text = query_params if query_params != None else ""
+        return render(request, "exchange/users.html", {"user_list": paginated_users, "search_text": search_text })
