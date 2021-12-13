cf3aaafe55a1b0e73459f3c05e018a8b4ff20c72
wkhelifi <wkhelifi@sfu.ca>
2021-12-12 19:41:28 -0800

    final styling required

--- sfu-exchange-project/exchange/forms.py
+++ sfu-exchange-project/exchange/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User, Tag, Answer
+from .models import User, Tag, Answer, Question
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import get_object_or_404
 
@@ -14,12 +14,12 @@ class RegistrationForm(UserCreationForm):
         fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2', 'faculty_id', 'bio']
 
 class QuestionForm(forms.Form):
-    title = forms.CharField(max_length=150)
+    title = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'title-field'}))
     # TODO: Max length of stackoverflow body is 30k characters, need to change model limit. : https://meta.stackexchange.com/questions/176445/knowing-your-limits-what-is-the-maximum-length-of-a-question-title-post-image
-    question_text = forms.CharField(max_length=3000)
-    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple)
+    question_text = forms.CharField(widget=forms.Textarea(attrs={'class': 'body-field'}))
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple(attrs={'class': 'tags-field'}))
     user_id = 1
-    anonymous = forms.BooleanField(required=False)
+    anonymous = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'anonymous-field'}))
     
     # TODO: Potentially provide validation if needed, otherwise we can remove these functions
     def clean_title(self):
@@ -30,6 +30,13 @@ class QuestionForm(forms.Form):
         data = self.cleaned_data['question_text']
         return data
 
+    class Meta:
+        model = Question
+
+
 class AnswerForm(forms.Form):
-    answer_text = forms.CharField(max_length=3000)
+    answer_text = forms.CharField(widget=forms.Textarea(attrs={'class': 'answer-field'}))
     anonymous = forms.BooleanField(required=False)
+
+    class Meta:
+        model = Answer
